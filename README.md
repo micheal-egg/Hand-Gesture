ad3aa2424dc3ba4e2cce290af88e246a55141881
Egboh, Micheal <m.egboh005@gmail.com>
2023-03-18 22:32:56 -0500

    Initial Commit with CV Implementation

---
+++ README.md
@@ -0,0 +1,29 @@
+# Hand Gesture Recognition ✋🤖
+
+Real-time hand gesture recognition system using **Python**, **OpenCV**, and **MediaPipe**.
+
+## 🚀 Features
+
+- Live webcam hand tracking
+- Detects hand landmarks
+- Recognizes gestures:
+  - Open Palm
+  - Fist
+  - Thumbs Up
+  - Peace Sign
+- Gesture-to-action mapping with on-screen feedback
+
+## 🛠 Tech Stack
+
+- Python
+- OpenCV
+- MediaPipe
+- NumPy
+
+## 📦 Installation
+
+```bash
+python -m venv venv
+venv\Scripts\activate
+pip install opencv-python mediapipe numpy
+```
---
+++ hand_gestures.py
@@ -0,0 +1,49 @@
+import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+mp_draw = mp.solutions.drawing_utils
+
+#Set up my webcam
+cap = cv2.VideoCapture(0)
+
+#Creates the hand tracker
+with mp_hands.Hands(
+    #It is a video, so false
+    static_image_mode=False,
+    #I will just use one hand for now
+    max_num_hands=1,
+    min_detection_confidence=0.6,
+    min_tracking_confidence=0.6
+) as hands:
+    #Continuously take from my webcam with a loop
+    while True:
+        #For reading my frame
+        ret, frame = cap.read()
+        #If it fails, break the loop
+        if not ret:
+            break
+
+        #For flipping camera, because I look better. 0 is for Vert Flip
+        frame = cv2.flip(frame, 1)  # mirror like FaceTime
+        #Convert the BGR image to RGB
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        #Process the RGB image to find hands
+        result = hands.process(rgb)
+
+        #If the hand is found, show the landmark things
+        if result.multi_hand_landmarks:
+            #For each detected hand(I used One) draw the dots and lines 
+            for hand_lms in result.multi_hand_landmarks:
+                mp_draw.draw_landmarks(frame, hand_lms, mp_hands.HAND_CONNECTIONS)
+
+        #Show the frame in the box called Camera Feed 
+        cv2.imshow("Camera Feed", frame)
+
+        #Makes it end with pressing q key 
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
